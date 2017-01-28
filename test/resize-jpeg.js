5513f4b5d6327721e0af747404899a0935aa1857
Kamil Dybicz <kamil@secretescapes.com>
2017-01-28 23:28:35 +0100

    fixing issue with assertion in callback not take into account because of use in async tests (only in resize JPEG tests for now) and upgrading sinon to 1.17.7

--- test/resize-jpeg.js
+++ test/resize-jpeg.js
@@ -3,44 +3,42 @@
 const ImageResizer = require("../libs/ImageResizer");
 const ImageData    = require("../libs/ImageData");
 const gm           = require("gm").subClass({ imageMagick: true });
-const tempfile     = require("tempfile");
 const test         = require("ava");
 const pify         = require("pify");
 const fs           = require("fs");
-const fsP          = pify(fs);
 
-test("Resize JPEG with cjpeg", async t => {
-    const fixture = await fsP.readFile(`${__dirname}/fixture/fixture.jpg`);
-	const destPath = tempfile(".jpg");
+let fixture;
+let image;
+
+test.before(async t => {
+    fixture = await pify(fs.readFile)(`${__dirname}/fixture/fixture.jpg`);
+    image   = new ImageData("fixture/fixture.jpg", "fixture", fixture);
+});
+
+test.cb("Resize JPEG with cjpeg", t => {
     const resizer = new ImageResizer({size: 200});
-    const image   = new ImageData("fixture/fixture.jpg", "fixture", fixture);
-
-    const resized = await resizer.exec(image);
-    await fsP.writeFile(destPath, resized.data);
-    gm(destPath).size((err, out) => {
-        if ( err ) {
-            t.fail(err);
-        } else {
-            t.is(out.width, 200);
-        }
-        fs.unlinkSync(destPath);
+    resizer.exec(image).then(result => {
+        gm(result.data).size((err, out) => {
+            if ( err ) {
+                t.fail(err);
+            } else {
+                t.is(out.width, 200);
+            }
+            t.end();
+        });
     });
 });
 
-test("Resize JPEG with jpegoptim", async t => {
-    const fixture = await fsP.readFile(`${__dirname}/fixture/fixture.jpg`);
-    const destPath = tempfile(".jpg");
+test.cb("Resize JPEG with jpegoptim", t => {
     const resizer = new ImageResizer({size: 200, jpegOptimizer: "jpegoptim"});
-    const image   = new ImageData("fixture/fixture.jpg", "fixture", fixture);
-
-    const resized = await resizer.exec(image);
-    await fsP.writeFile(destPath, resized.data);
-    gm(destPath).size((err, out) => {
-        if ( err ) {
-            t.fail(err);
-        } else {
-            t.is(out.width, 200);
-        }
-        fs.unlinkSync(destPath);
+    resizer.exec(image).then(result => {
+        gm(result.data).size((err, out) => {
+            if ( err ) {
+                t.fail(err);
+            } else {
+                t.is(out.width, 200);
+            }
+            t.end();
+        });
     });
 });
