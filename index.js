ac8fe215620749518e2bb412f96bb771d978acb5
Kamil Dybicz <kamil@secretescapes.com>
2017-02-16 22:45:25 +0100

    adding support for sns messages and example SNS event file

--- index.js
+++ index.js
@@ -13,8 +13,27 @@ const fs             = require("fs");
 const path           = require("path");
 
 // Lambda Handler
-exports.handler = (event, context) => {
-    const s3Object   = event.Records[0].s3;
+exports.handler = (event, context, callback) => {
+
+    var eventRecord = event.Records && event.Records[0];
+    if (eventRecord) {
+        if (eventRecord.eventSource === 'aws:s3' && eventRecord.s3) {
+            console.log("Incoming S3 event...");
+            process(eventRecord.s3, callback);
+        } else if (eventRecord.EventSource === 'aws:sns' && eventRecord.Sns) {
+            console.log("Incoming SNS message...");
+            process(JSON.parse(event.Records[0].Sns.Message).Records[0].s3, callback);
+        } else {
+            console.log(JSON.stringify(eventRecord));
+            callback('unsupported event source');
+        }
+    } else {
+        console.log(JSON.stringify(event));
+        callback('no records in the event');
+    }
+};
+
+function process(s3Object, callback) {
     const configPath = path.resolve(__dirname, "config.json");
     const processor  = new ImageProcessor(s3Object);
     const config     = new Config(
@@ -23,16 +42,16 @@ exports.handler = (event, context) => {
 
     processor.run(config)
     .then((processedImages) => {
-        var message = "OK, " + processedImages + " images were processed.";
+        const message = "OK, " + processedImages + " images were processed.";
         console.log(message);
-        context.succeed(message);
+        callback(null, message);
     })
     .catch((messages) => {
         if ( messages === "Object was already processed." ) {
             console.log("Image already processed");
-            context.succeed("Image already processed");
+            callback(null, "Image already processed");
         } else {
-            context.fail("Error processing " + s3Object.object.key + ": " + messages);
+            callback("Error processing " + s3Object.object.key + ": " + messages);
         }
     });
-};
+}
