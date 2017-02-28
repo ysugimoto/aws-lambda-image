e6e0424a459bf849b812969c18c1454a3298b6f8
Kamil Dybicz <kamil@secretescapes.com>
2017-02-28 21:33:27 +0100

    split all test cases into separate tests

--- test/image-data.js
+++ test/image-data.js
@@ -3,78 +3,108 @@
 const ImageData = require("../lib/ImageData");
 const test      = require("ava");
 
-test("Build output path", t => {
-    const image = new ImageData("a/b/c/key.png", "bucket", "data", {});
+let image;
 
-    // No directory
+test.before(t => {
+    image = new ImageData("a/b/c/key.png", "bucket", "data", {});
+});
+
+test("Build output path when directory is undefined", t => {
     t.is(image.combineWithDirectory(undefined), "a/b/c/key.png");
+});
 
-    // Empty directory
+test("Build output path when directory is empty", t => {
     t.is(image.combineWithDirectory(""), "key.png");
+});
 
-    // Relative directory
+test("Build output path when directory is relative deeper", t => {
     t.is(image.combineWithDirectory("./d"), "a/b/c/d/key.png");
+});
 
-    // Internal directory
+test("Build output path when directory is relative deeper - 2nd level", t => {
     t.is(image.combineWithDirectory("./d/e"), "a/b/c/d/e/key.png");
+});
 
-    // External directory
+test("Build output path when directory is relative backward", t => {
     t.is(image.combineWithDirectory(".."), "a/b/key.png");
+});
 
-    // External internal directory
+test("Build output path when directory is relative backward with new subdirectory branch", t => {
     t.is(image.combineWithDirectory("../d"), "a/b/d/key.png");
+});
 
-    // Root directory
+test("Build output path when directory is absolute", t => {
     t.is(image.combineWithDirectory("d"), "d/key.png");
+});
 
-    // Root internal directory
+test("Build output path when directory is absolute - 2nd level", t => {
     t.is(image.combineWithDirectory("d/e"), "d/e/key.png");
+});
 
-    // With prefix
+test("Build output path with prefix", t => {
     t.is(image.combineWithDirectory("d/e", "prefix-"), "d/e/prefix-key.png");
+});
 
-    // With suffix
+test("Build output path with suffix", t => {
     t.is(image.combineWithDirectory("d/e", "", "-suffix"), "d/e/key-suffix.png");
+});
 
-    // With prefix and suffix
+test("Build output path with prefix and suffix", t => {
     t.is(image.combineWithDirectory("d/e", "prefix-", "_suffix"), "d/e/prefix-key_suffix.png");
 });
 
-test("Build output path with RegEx", t => {
-    const image = new ImageData("a/b/c/key.png", "bucket", "data", {});
-
-    // No directory
+test("[RegEx] Build output path when directory is an empty object", t => {
     t.is(image.combineWithDirectory({}), "a/b/c/key.png");
+});
+
+test("[RegEx] Build output path when directory is an empty regex map", t => {
     t.is(image.combineWithDirectory({regex: {}}), "a/b/c/key.png");
+});
+
+test("[RegEx] Build output path when directory is an regex map with find and replace keys empty", t => {
     t.is(image.combineWithDirectory({regex: {find: "", replace: ""}}), "a/b/c/key.png");
+});
 
-    // Empty directory
+test("[RegEx] Build output path when directory is an regex map with replace keys is missing", t => {
+    t.is(image.combineWithDirectory({regex: {find: ""}}), "a/b/c/key.png");
+});
+
+test("[RegEx] Build output path when regex replace whole directory", t => {
     t.is(image.combineWithDirectory({regex: {find: ".*", replace: ""}}), "key.png");
+});
 
-    // Relative directory
+test("[RegEx] Build output path when regex adds subdirectory", t => {
     t.is(image.combineWithDirectory({regex: {find: "$", replace: "/d"}}), "a/b/c/d/key.png");
+});
 
-    // Internal directory
+test("[RegEx] Build output path when regex adds subdirectory - 2nd level", t => {
     t.is(image.combineWithDirectory({regex: {find: "$", replace: "/d/e"}}), "a/b/c/d/e/key.png");
+});
 
-    // External directory
+test("[RegEx] Build output path when regex removes top subdirectory", t => {
     t.is(image.combineWithDirectory({regex: {find: "[a-zA-Z]+$", replace: ""}}), "a/b/key.png");
+});
 
-    // External internal directory
+test("[RegEx] Build output path when regex replaces top subdirectory with new one", t => {
     t.is(image.combineWithDirectory({regex: {find: "\/c", replace: "/d"}}), "a/b/d/key.png");
+});
 
-    // Root directory
+test("[RegEx] Build output path when regex replaces old path with new absolute one", t => {
     t.is(image.combineWithDirectory({regex: {find: ".*", replace: "d"}}), "d/key.png");
+});
 
-    // Root internal directory
+test("[RegEx] Build output path when regex replaces old path with new absolute one - 2nd level", t => {
     t.is(image.combineWithDirectory({regex: {find: ".*", replace: "d/e"}}), "d/e/key.png");
+});
 
-    // With prefix
+test("[RegEx] Build output path with regex and prefix", t => {
     t.is(image.combineWithDirectory({regex: {find: ".*", replace: "d/e"}}, "prefix-"), "d/e/prefix-key.png");
+});
 
-    // With suffix
+test("[RegEx] Build output path with regex and suffix", t => {
     t.is(image.combineWithDirectory({regex: {find: ".*", replace: "d/e"}}, "", "-suffix"), "d/e/key-suffix.png");
+});
 
-    // With prefix and suffix
+test("[RegEx] Build output path with regex, prefix and suffix", t => {
     t.is(image.combineWithDirectory({regex: {find: ".*", replace: "d/e"}}, "prefix-", "_suffix"), "d/e/prefix-key_suffix.png");
 });
